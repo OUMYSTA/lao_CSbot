1de0744aeffa28bd1b87d24be85631e6a9d39a35
OUMYSTA <oum1234thakhek@gmail.com>
2024-06-30 21:02:23 +0700

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <HeadIntroVue />
+    <ChatPage />
+    <FooTerVue />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatPage from './components/ChatPage.vue'
+import FooTerVue from './components/FooTer.vue';
+import HeadIntroVue from './components/HeadIntro.vue';
+</script>
